fae4f5bdd38ee01ddb8ec8511bc7d0b5a7b42d18
Russell Gordon <rgordon@lcs.on.ca>
2023-05-27 06:51:42 -0400

    Initial Commit

---
+++ FavouriteMovies/FavouriteMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteMoviesApp.swift
+//  FavouriteMovies
+//
+//  Created by Russell Gordon on 2023-05-27.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
