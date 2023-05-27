b98111cebee353722d296c707740bc325925c9a9
Russell Gordon <rgordon@lcs.on.ca>
2023-05-27 06:53:11 -0400

    Completed initial project organization.

---
+++ FavouriteMovies/Views/MoviesListView.swift
@@ -0,0 +1,20 @@
+//
+//  MoviesListView.swift
+//  FavouriteMovies
+//
+//  Created by Russell Gordon on 2023-05-27.
+//
+
+import SwiftUI
+
+struct MoviesListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MoviesListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MoviesListView()
+    }
+}
