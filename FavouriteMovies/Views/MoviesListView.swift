354a0ef1800186f64d74c2847fba0b18d7ae36b2
Russell Gordon <rgordon@lcs.on.ca>
2023-05-27 08:23:51 -0400

    Made the list of movies read from the database instead of using a static layout.

--- FavouriteMovies/Views/MoviesListView.swift
+++ FavouriteMovies/Views/MoviesListView.swift
@@ -5,28 +5,25 @@
 //  Created by Russell Gordon on 2023-05-27.
 //
 
+import Blackbird
 import SwiftUI
 
 struct MoviesListView: View {
 
     // MARK: Stored properties
     
+    // The list of favourite movies, as read from the database
+    @BlackbirdLiveModels({ db in
+        try await Movie.read(from: db)
+    }) var movies
+    
     // MARK: Computed properties
     var body: some View {
         NavigationView {
-            List {
-                MovieItemView(name: "E.T. the Extra-Terrestrial",
-                              genre: "Science Fiction",
-                              rating: 4)
-
-                MovieItemView(name: "Ferris Bueller's Day Off",
-                              genre: "Comedy",
-                              rating: 4)
-
-                MovieItemView(name: "Ghostbusters",
-                              genre: "Comedy",
-                              rating: 5)
-
+            List(movies.results) { currentMovie in
+                MovieItemView(name: currentMovie.name,
+                              genre: currentMovie.genre,
+                              rating: currentMovie.rating)
             }
             .navigationTitle("Favourite Movies")
         }
@@ -36,5 +33,7 @@ struct MoviesListView: View {
 struct MoviesListView_Previews: PreviewProvider {
     static var previews: some View {
         MoviesListView()
+        // Make the database available to all other view through the environment
+        .environment(\.blackbirdDatabase, AppDatabase.instance)
     }
 }
