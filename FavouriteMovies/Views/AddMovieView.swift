cd20c6cad0f12aaf2427c725a4f46e9f502cba4c
Russell Gordon <rgordon@lcs.on.ca>
2023-05-28 08:04:39 -0400

    Refactored (cleaned up) the code in AddMovieView so that the logic to add a movie is kept in a function.

--- FavouriteMovies/Views/AddMovieView.swift
+++ FavouriteMovies/Views/AddMovieView.swift
@@ -5,11 +5,16 @@
 //  Created by Russell Gordon on 2023-05-28.
 //
 
+import Blackbird
 import SwiftUI
 
 struct AddMovieView: View {
     
     // MARK: Stored properties
+    
+    // Access the connection to the database (needed to add a new record)
+    @Environment(\.blackbirdDatabase) var db: Blackbird.Database?
+    
     // Holds details for the new movie
     @State var name = ""
     @State var genre = ""
@@ -39,12 +44,55 @@ struct AddMovieView: View {
                 Spacer()
             }
             .padding(5)
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button(action: {
+                        addMovie()
+                    }, label: {
+                        Text("Add")
+                    })
+                }
+            }
         }
     }
+    
+    // MARK: Functions
+    
+    func addMovie() {
+        // Write to database
+        Task {
+            try await db!.transaction { core in
+                try core.query("""
+                            INSERT INTO movie (
+                                name,
+                                genre,
+                                rating
+                            )
+                            VALUES (
+                                (?),
+                                (?),
+                                (?)
+                            )
+                            """,
+                            name,
+                            genre,
+                            rating)
+            }
+            // Reset input fields after writing to database
+            name = ""
+            genre = ""
+            rating = 3
+        }
+
+    }
+    
 }
 
 struct AddMovieView_Previews: PreviewProvider {
     static var previews: some View {
         AddMovieView()
+        // Make the database available to all other view through the environment
+        .environment(\.blackbirdDatabase, AppDatabase.instance)
+
     }
 }
